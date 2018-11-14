d2cbf3d97809f45f1b2a1c3f3b781e144703f006
Michael Koltasheu <M.Koltasheu@synergetic.ag>
2018-11-14 21:26:31 +0100

    initial commit

---
+++ Kubernetes.TestService/Controllers/ApplicationStatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kubernetes.TestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationStatusController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<int> Get()
+        {
+            return Ok();
+        }
+    }
+}
