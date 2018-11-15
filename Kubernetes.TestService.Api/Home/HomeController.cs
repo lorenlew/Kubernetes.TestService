01d8bdb670396cfa07186a3e6b1f66aad0c30b52
Mikhail Koltasheu <mikhail.koltasheu@zuehlke.com>
2018-11-15 16:54:33 +0100

    Added test html view to test css links with ingress path routing

---
+++ Kubernetes.TestService.Api/Home/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kubernetes.TestService.Api.Home
+{
+	[Route("/")]
+	[Controller]
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public ActionResult Index()
+		{
+
+			return View("~/Home/Home.cshtml", $"You've hit {Environment.MachineName} \n");
+		}
+	}
+}
